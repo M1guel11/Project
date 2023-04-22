c627590f2ee18bc587a3b89f0c7f9cff513ed7fd
M1guel11 <marquesmiguel11@gmail.com>
2023-04-22 22:24:19 +0100

    improveming code

--- src/main.ml
+++ src/main.ml
@@ -1,8 +1,8 @@
 open Scanf
 open Printf
 
-let em_par a b c = ((a, b), c)
-let em_par_2 a b c = (([ a ], [ b ]), [ c ])
+
+let em_par a b c = (([ a ], [ b ]), [ c ])
 
 let rec remove_duplicates l =
   let rec contains l n =
@@ -14,12 +14,54 @@ let rec remove_duplicates l =
       let acc = remove_duplicates t in
       if contains acc h then acc else h :: acc
 
+
+      let rec sublist sub lst =
+        match sub, lst with
+        | [], _ -> true
+        | _, [] -> false
+        | x::xs, y::ys ->
+            if x = y then sublist xs ys
+            else sublist sub ys
+      
+
+
+
+
+      let is_sublist sub lst =
+        let rec check_sublist sub lst =
+          match sub, lst with
+          | [], _ -> true
+          | _, [] -> false
+          | x::xs, y::ys -> if x = y then check_sublist xs ys else check_sublist sub ys
+        in
+        let sub_length = List.length sub in
+        let rec check_lists lists =
+          match lists with
+          | [] -> false
+          | hd::tl -> if List.length hd >= sub_length && check_sublist sub hd then true else check_lists tl
+        in
+        check_lists lst
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let create_maquina c =
   let i = ref 0 in
   let input = ref [] in
   while !i < c do
     let str = read_line () in
-    input := sscanf str " %d %c %d " em_par :: !input;
+    input := sscanf str " %d %c %d " em_par:: !input;
     i := !i + 1
   done;
   !input
@@ -58,90 +100,79 @@ let read_int_list () = List.map (fun x -> int_of_string x) (read_line () |> Stri
 let rec possiveis label transicoes ret =
   match transicoes with
   | ((a, b), c) :: tl ->
-      if a = label then possiveis label tl (ret @ [ (b, c) ])
+      if List.mem label a then possiveis label tl (ret @ [ (b, c) ])
       else possiveis label tl ret
   | [] -> List.sort compare ret
 
-let check_2_poss poss1 poss2 finl =
+
+let val1 = possiveis 3 trans []
+let val2 = possiveis 5 trans []
+
+
+let compare_possibles poss1 poss2 finl =
   let a, b = poss1 in
   let x, y = poss2 in
-  if a = x && b = y then true
-  else if a = x && List.mem b finl && List.mem y finl then true
-  else false
+   (List.compare  compare a x == 0  && List.compare compare b y == 0 || List.compare compare a x == 0 && sublist b finl && sublist y finl ) 
 
-let rec check_poss_in_list poss lposs finl =
-  match lposs with
-  | x :: tl ->
-      if check_2_poss poss x finl then true else check_poss_in_list poss tl finl
-  | [] -> false
+  
 
-let rec check_2_lposs lposs1 lposs2 finl =
-  match lposs1 with
-  | x :: tl ->
-      if check_poss_in_list x lposs2 finl then check_2_lposs tl lposs2 finl
-      else false
-  | [] -> true
 
-let possiveis_equivaletes lposs1 lposs2 finl =
-  if List.length lposs1 = List.length lposs2 then
-    check_2_lposs lposs1 lposs2 finl
-  else false
+let rec compare_lists_possibles l1 l2 finl =
+   if List.length l1 = List.length l2 then
+      List.fold_left2 (fun acc x y ->
+        if acc = false then false
+        else compare_possibles x y finl
+      ) true (List.sort compare l1)  (List.sort compare l2)
+    
+   else false
 
-let estado_equi state_1 state_2 finl transicoes =
-    possiveis_equivaletes
-      (possiveis state_1 transicoes [])
-      (possiveis state_2 transicoes [])
-      finl
 
-let rec combinacoes_to_state combinacoes ret finl transicoes =
-  match combinacoes with
+
+
+
+let rec combinacoes lista ret =
+  let rec comb lista ret x =
+    match lista with [] -> ret | y :: tl -> comb tl (ret @ [ (x, y) ]) x
+  in
+    match lista with
+      | [] -> ret
+      | x :: tl -> combinacoes tl (ret @ comb tl [] x)
+
+
+let sas= combinacoes (initl @ finl) []  
+
+
+let rec combinacoes_to_state list ret finl transicoes =
+  match list with
   | (a, b) :: tl ->
-      if estado_equi a b finl transicoes then [ a; b ] :: ret
-      else combinacoes_to_state tl ret finl transicoes
-  | [] -> ret
+      if compare_lists_possibles (possiveis a transicoes []) (possiveis b transicoes []) finl then combinacoes_to_state tl ([a; b] :: ret) finl transicoes 
+      else 
+        begin 
+          if is_sublist [a] ret || is_sublist [b] ret then combinacoes_to_state tl ( ret) finl transicoes  
+          else combinacoes_to_state tl (ret @ [ [a]; [b] ]) finl transicoes 
+        end 
+  | [] -> List.sort compare  (remove_duplicates ret)
+   
 
-let rec combinacoes lista ret x =
-  match lista with [] -> ret | y :: tl -> combinacoes tl (ret @ [ (x, y) ]) x
+let yyy= combinacoes_to_state sas [] finl trans
 
-let rec combinacoes_list lista ret =
-  match lista with
-  | [] -> ret
-  | x :: tl -> combinacoes_list tl (ret @ combinacoes tl [] x)
+let transform_list states trans =
+  let new_st = List.concat b in
+  List.map (fun ((st, sym), tr) ->
+    if List.mem (List.hd st) new_st then
+      ((new_st, sym), tr)
+    else
+      ((st, sym), tr)) a
 
-let rec exists x new_states =
-  match new_states with
-  | hd :: tl -> if List.mem x hd then true else exists x tl
-  | [] -> false
 
-let rec distinguish transicoes estados finl ret =
-  match estados with
-  | hd :: tl ->
-      distinguish transicoes tl finl
-        (ret @ combinacoes_to_state (combinacoes_list hd []) [] finl transicoes)
-  | [] -> ret
 
-let rec new_automato old_states new_states ret =
-  match old_states with
-  | hd :: tl ->
-      if exists hd new_states then new_automato tl new_states ret
-      else new_automato tl new_states ([ hd ] :: ret)
-  | [] -> List.rev ret @ new_states
+let asdas= transform_list trans yyy
 
 let rec get_buddy x new_states =
   match new_states with
   | hd :: tl -> if List.mem (List.hd x) hd then hd else get_buddy x tl
   | [] -> x
 
-let rec new_trans old_trans ret =
-  match old_trans with
-  | ((a, b), c) :: tl -> new_trans tl (em_par_2 a b c :: ret)
-  | [] -> ret
-
-(* let rec check_list_list_of_lists list big_list =
-   match big_list with
-   | hd :: tl ->
-       if compare hd list = 0 then true else check_list_list_of_lists list tl
-   | [] -> false *)
 
 let rec get_labels trans x ret =
   match trans with
@@ -178,11 +209,11 @@ let _print_result =
   printf "%d\n" init;
   print_list initl;
   printf "%d\n" nfinl;
-  print_list finl;
-  print_trans trans
+  print_list finl
+  (*print_trans trans*)
 
 
-
+(*
 let minimized_automato =
   new_automato (initl @ finl) (distinguish trans [ initl; finl ] finl []) []
 
@@ -191,3 +222,4 @@ let _u =
     (remove_duplicates
        (final_trans_list (new_trans trans []) minimized_automato []))
     [] []
+*)
